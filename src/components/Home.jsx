e8489d32afbabcf3ea6edc422af7644da77c1112
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-04 11:33:09 -0500

    feat: React Router implemented

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import '@styles/App.scss'
+import '@styles/Home.scss'
 
 const Home = () => {
   return (
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import Home from '@components/Home'
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
     </>
   )
 }
