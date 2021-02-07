bb5f682fce2fde6e0004af5bec59a3bf3ca8dafb
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-06 22:49:53 -0500

    feat(Home): form implementation, WIP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import '@styles/Home.scss'
 
 const Home = () => {
+  const searchRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(searchRef.current)
+  }
+
   return (
     <div className='home'>
-      Hello World!
+      <h1>Search</h1>
+
+      <form className='home__searchbar' ref={searchRef}>
+        <input type='text' name='search' />
+        <button type='submit' onSubmit={handleSubmit}>Search</button>
+      </form>
+
+      {/* <div className='result'></div> */}
     </div>
   )
 }
